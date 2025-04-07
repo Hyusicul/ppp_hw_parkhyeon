a2d6b33cd4e68d6dabb402cc361d4a9375b1b19f
Park Hyeon <105685800+Hyusicul@users.noreply.github.com>
2025-04-07 15:56:12 +0900

    Delete 프원실_과제04_타입_202210072_박현 directory

--- 프원실_과제04_타입_202210072_박현/ppp_hw04_calories.py
+++
@@ -1,6 +0,0 @@
-calories=[26, 40, 297] #빨간 파프리카, 머스크멜론, 건포도도
-red_paprika=int(input("빨간 파프리카를 얼만큼 드셨습니까? (100g단위)"))
-musk_melon=int(input("머스크 멜론을 얼만큼 드셨습니까? (100g단위)"))
-raisin=int(input("건포도를 얼만큼 드셨습니까? (100g단위)"))
-total_calories=calories[0]*(red_paprika/100)+calories[1]*(musk_melon/100)+calories[2]*(raisin/100)
-print("당신은 총 {}kcal를 섭취하셨군요".format(total_calories))
--- 프원실_과제04_타입_202210072_박현/ppp_hw04_circle.py
+++
@@ -1,8 +0,0 @@
-import math
-r=int(input("원의 반지름의 길이는?"))
-circumference=r*2*math.pi
-circle_area=pow(r,2)*math.pi
-print("="*30)
-print("원의 둘레는{:.1f}".format(circumference))
-print("원의 넓이는{:.2f}".format(circle_area))
-print("="*30)
