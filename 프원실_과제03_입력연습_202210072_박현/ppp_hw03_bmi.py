05f468fc714150ea05ad57ed8fd91489ca88d70e
Park Hyeon <105685800+Hyusicul@users.noreply.github.com>
2025-04-07 15:56:06 +0900

    Delete 프원실_과제03_입력연습_202210072_박현 directory

--- 프원실_과제03_입력연습_202210072_박현/ppp_hw03_bmi.py
+++
@@ -1,6 +0,0 @@
-import math
-height=int(input("키는 몇입니까?(cm)"))
-weight=int(input("몸무게는 몇입니까?(kg)"))
-height_m=height/100
-bmi=weight/math.pow(height/100,2)
-print("키{}cm에 몸무게가 {}kg인 사람의 bmi는 {}입니다.".format(height, weight, bmi))
--- 프원실_과제03_입력연습_202210072_박현/ppp_hw03_trapezoid.py
+++
@@ -1,6 +0,0 @@
-import math
-a=int(input("사다리꼴 밑변의 길이는?"))
-b=int(input("사다리꼴 윗변의 길이는?"))
-h=int(input("사다리꼴 높이는?"))
-trapezoide_area=((a+b)*h)/2
-print("밑변이 {}이고 윗변이 {}, 높이가 {}인 사다리꼴의 넓이는 {}이다.".format(a, b, h, trapezoide_area))
